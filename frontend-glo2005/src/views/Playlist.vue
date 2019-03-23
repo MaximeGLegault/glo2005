2c85b88892e0f41aada660031d65025126f14dea
François-JosephLacroix <fjlacroix@gmail.com>
2019-03-23 18:25:32 -0400

    Created fake albums. Created Playlist and Song components

---
+++ frontend-glo2005/src/views/Playlist.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul>
+        <song v-for="song of songs" v-bind:key="song.title" title="song.title" artist="song.artist" album="song.album" genre="song.genre" duration="song.duration" year="song.year"/>
+    </ul>
+</template>
+
+<script>
+    import Song from './Song';
+    export default {
+        name: "Playlist",
+        props: {
+            songs: Array
+        },
+        methods:{
+
+        }
+    };
+
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend-glo2005/src/views/Song.vue
@@ -0,0 +1,49 @@
+<template>
+    <li class="song">
+        <div class="title">
+            {{title}}
+        </div>
+        <div class="artist">
+            {{artist}}
+        </div>
+        <div class="album">
+            {{album}}
+        </div>
+        <div class="genre">
+            {{genre}}
+        </div>
+        <div class="duration">
+            {{duration}}
+        </div>
+        <div class="year">
+            {{year}}
+        </div>
+    </li>
+</template>
+
+<script>
+    export default {
+        name: "Song",
+        props: {
+            title: String,
+            artist: String,
+            album: String,
+            genre: String,
+            duration: Number,
+            year: Number,
+        },
+        methods:{
+
+        }
+    };
+</script>
+
+<style scoped>
+
+    song {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+
+    }
+</style>
