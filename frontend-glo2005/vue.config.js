b4e71d8697c5413b706a287659411877d3c96e7a
Maxime Gagnon-Legault <maxime.glegault@gmail.com>
2019-04-08 10:17:44 -0400

    remove /static in the url

--- frontend-glo2005/vue.config.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//     publicPath: '/static/',
-//     outputDir: undefined,
-//     assetsDir: undefined,
-//     runtimeCompiler: undefined,
-//     productionSourceMap: undefined,
-//     parallel: undefined,
-//     css: undefined
-// }
