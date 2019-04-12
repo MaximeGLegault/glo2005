f977cff139db6bb436388570b9a7b73a997270ac
Maxime Gagnon-Legault <maxime.glegault@gmail.com>
2019-04-11 22:29:51 -0400

    docker-compose now works

--- flask/infrastructure/persistence/database_initialisation.py
+++ flask/infrastructure/persistence/database_initialisation.py
@@ -1,7 +1,8 @@
 from mysql.connector import connect
 
 # host = "192.168.99.100"
-host = "localhost"
+# host = "localhost"
+host = "bd"  # pour le docker-compose
 port = "3306"
 username = "root"
 password = "1234"
--- flask/routes/home.py
+++ flask/routes/home.py
@@ -1,9 +1,9 @@
+from datetime import date, datetime
 from flask import Blueprint, render_template
 
 home = Blueprint('home', __name__, url_prefix='/')
 
 
-@home.route('/', defaults={'path': ''})
-@home.route('/<path:path>')
-def main_route(path):
-    return render_template("index.html")
+@home.route('/')
+def main_route():
+    return str(datetime.utcnow())
